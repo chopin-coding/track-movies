49e5954e216fc84a73eade8e4381514406485dcf
Frosty <emreerguvan@gmail.com>
2023-06-01 01:33:26 +0400

    added validations to update and create parameters

--- app/dto/movie.py
+++ app/dto/movie.py
@@ -13,24 +13,24 @@ class CreateMovieBody(BaseModel):
 
     @validator("title")
     def title_length_gt_one(cls, v):
-        if len(v) < 2:
+        if not 221 > len(v) > 2:
             raise ValueError(
-                "The movie title length has to be greater than 1 character"
+                "The movie title should be 2 to 220 characters long."
             )
         return v
 
     @validator("description")
     def description_length_gt_one(cls, v):
-        if len(v) < 2:
+        if not 5001 > len(v) > 2:
             raise ValueError(
-                "The movie description length has to be greater than 1 character"
+                "The movie title should be 2 to 5000 characters long."
             )
         return v
 
     @validator("release_year")
     def release_year_length_gt_(cls, v):
-        if v < 1894:
-            raise ValueError("The movie release year has to be after 1893")
+        if not 2101 > v > 1894:
+            raise ValueError("The movie release year should be between 1894 and 2100.")
         return v
 
 
@@ -51,13 +51,34 @@ class MovieResponseWithCount(BaseModel):
 
 
 class MovieUpdateBody(BaseModel):
-    # TODO: Implement the validations of the CreateMovieBody class to this class as well, and any other
-    #  validations that may be necessary.
-
     title: typing.Optional[str] = None
     description: typing.Optional[str] = None
     release_year: typing.Optional[int] = None
     watched: typing.Optional[bool] = None
+
+    @validator("title")
+    def title_length_gt_one(cls, v):
+        if not 221 > len(v) > 2:
+            raise ValueError(
+                "The movie title should be 2 to 220 characters long."
+            )
+        return v
+
+    @validator("description")
+    def description_length_gt_one(cls, v):
+        if not 5001 > len(v) > 2:
+            raise ValueError(
+                "The movie title should be 2 to 5000 characters long."
+            )
+        return v
+
+    @validator("release_year")
+    def release_year_length_gt_(cls, v):
+        if not 2101 > v > 1894:
+            raise ValueError("The movie release year should be between 1894 and 2100.")
+        return v
+
+
 
 
 class MovieDeleteResponse(BaseModel):
